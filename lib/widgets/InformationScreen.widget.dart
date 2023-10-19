4ec1281aac3062d114a75a1c9067553488d0124a
Mahmoud Nabhan <niypoo@gmail.com>
2023-10-19 11:01:58 +0300

    Update InformationScreen.widget.dart

--- lib/widgets/InformationScreen.widget.dart
+++ lib/widgets/InformationScreen.widget.dart
@@ -2,7 +2,7 @@ import 'package:calculator_module/widgets/calculatorInformationScreen.widget.dar
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:number_picker_module/numberPicker.controller.dart';
-import 'package:auto_size_text/auto_size_text.dart';
+// import 'package:auto_size_text/auto_size_text.dart';
 
 class InformationScreen extends GetView<NumberPickerController> {
   const InformationScreen({
@@ -19,12 +19,12 @@ class InformationScreen extends GetView<NumberPickerController> {
         // LABELs
         label: controller.initData.label,
 
-        // TITLE
-        title: AutoSizeText(
-          controller.initData.title,
-          style: Get.theme.textTheme.bodyLarge,
-          maxLines: 1,
-        ),
+        // // TITLE
+        // title: AutoSizeText(
+        //   controller.initData.title,
+        //   style: Get.theme.textTheme.bodyLarge,
+        //   maxLines: 1,
+        // ),
       ),
     );
   }
